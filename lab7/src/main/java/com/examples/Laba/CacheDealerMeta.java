bdc152f717296d71e2868d91301a3011ff72981d
Egorychev Artem <temych_2000@mail.ru>
2019-12-13 23:20:01 +0300

    Scripted auto-commit on change (2019-12-13 23:20:01) by gitwatch.sh

--- lab7/src/main/java/com/examples/Laba/CacheDealerMeta.java
+++ lab7/src/main/java/com/examples/Laba/CacheDealerMeta.java
@@ -6,5 +6,9 @@ public class CacheDealerMeta {
     ZFrame id;
     long lastBeat;
     int start, end;
-    
+
+    public CacheDealerMeta(ZFrame id){
+        this.id = id;
+        this.lastBeat = System.currentTimeMillis();
+    }
 }
