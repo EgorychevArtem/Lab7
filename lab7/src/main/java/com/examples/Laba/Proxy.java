43081cfc6379faee8800bd507ff8a8481548d01f
Egorychev Artem <temych_2000@mail.ru>
2019-12-13 21:45:44 +0300

    Scripted auto-commit on change (2019-12-13 21:45:44) by gitwatch.sh

--- lab7/src/main/java/com/examples/Laba/Proxy.java
+++ lab7/src/main/java/com/examples/Laba/Proxy.java
@@ -11,13 +11,14 @@ public class Proxy implements Closeable {
     public ZContext context;
     public ZMQ.Socket clientRouter, cacheRouter, poller;
     public CacheDealerStorage dealers;
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         try (
                 ZContext context = new ZContext();
                 Proxy proxy = new Proxy(context)
-        ){
+        ) {
 
         }
+    }
 
         public Proxy(ZContext context){
             this.context = context;
