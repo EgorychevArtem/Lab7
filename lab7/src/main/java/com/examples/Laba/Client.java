facb157ed90d230a79ec264841303af1c827c378
Egorychev Artem <temych_2000@mail.ru>
2019-12-13 21:35:39 +0300

    Scripted auto-commit on change (2019-12-13 21:35:39) by gitwatch.sh

--- lab7/src/main/java/com/examples/Laba/Client.java
+++ lab7/src/main/java/com/examples/Laba/Client.java
@@ -23,7 +23,7 @@ public class App {
             result.addString(msg);
             result.send(client);
 
-            ZMsg request = ZMsg.recvMsg(client);
+            ZMsg request = ZMsg.recvMsg(clien
             System.out.println(request.toString());
             request.destroy();
         }
