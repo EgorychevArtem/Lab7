17c6d30d00f7dd86641eef4fb0869c6c809ee4df
Egorychev Artem <temych_2000@mail.ru>
2019-12-13 23:22:50 +0300

    Scripted auto-commit on change (2019-12-13 23:22:50) by gitwatch.sh

--- lab7/src/main/java/com/examples/Laba/CacheDealerStorage.java
+++ lab7/src/main/java/com/examples/Laba/CacheDealerStorage.java
@@ -1,7 +1,15 @@
 package com.examples.Laba;
 
+import org.zeromq.ZFrame;
+
 import java.util.Map;
+import java.util.Optional;
+import java.util.TreeMap;
 
 public class CacheDealerStorage {
-    Map<String, CacheDealerMeta>
+    Map<String, CacheDealerMeta> dealers = new TreeMap<>();
+
+    public Optional<ZFrame> getDealerId(int id){
+        
+    }
 }
