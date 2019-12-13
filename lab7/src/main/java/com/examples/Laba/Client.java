d1f91d06d303dc84d3d4e34771230482bac3af3c
Egorychev Artem <temych_2000@mail.ru>
2019-12-13 21:38:28 +0300

    Scripted auto-commit on change (2019-12-13 21:38:28) by gitwatch.sh

--- lab7/src/main/java/com/examples/Laba/Client.java
+++ lab7/src/main/java/com/examples/Laba/Client.java
@@ -15,22 +15,10 @@ public class Client {
             Client client = new Client(context)
         )
 
-        ZMQ.Socket client = zContext.createSocket(SocketType.REQ);
-        client.setHWM(0);
-        client.connect("localhost:8080");
-        System.out.println("Connect to client");
 
-        Scanner in = new Scanner(System.in);
-
-        while(true){
-            String msg = in.nextLine();
-            ZMsg result = new ZMsg();
-            result.addString(msg);
-            result.send(client);
-
-            ZMsg request = ZMsg.recvMsg(clien
-            System.out.println(request.toString());
-            request.destroy();
+         public Client(ZContext context) {
+                this.context = context;
         }
+
     }
 }
