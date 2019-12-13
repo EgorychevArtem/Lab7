40102a46c5d9c8ac4ec4e91c82ce7c86a012f563
Egorychev Artem <temych_2000@mail.ru>
2019-12-13 18:59:55 +0300

    Scripted auto-commit on change (2019-12-13 18:59:54) by gitwatch.sh

--- lab7/src/main/java/com/examples/Laba/App.java
+++ lab7/src/main/java/com/examples/Laba/App.java
@@ -1,7 +1,11 @@
 package com.examples.Laba;
 
+import org.zeromq.ZContext;
+import org.zeromq.ZMQ;
+
 public class App {
     public static void main(String[] args){
-        
+        ZContext zContext = new ZContext();
+        ZMQ.Socket
     }
 }
