848582805a7bf5cb9bc923e32c52233e0e5f2e93
Egorychev Artem <temych_2000@mail.ru>
2019-12-14 01:02:49 +0300

    Scripted auto-commit on change (2019-12-14 01:02:49) by gitwatch.sh

--- lab7/src/main/java/com/examples/Laba/CacheDealer.java
+++ lab7/src/main/java/com/examples/Laba/CacheDealer.java
@@ -1,6 +1,11 @@
 package com.examples.Laba;
 
 import java.io.Closeable;
+import java.io.IOException;
 
 public class CacheDealer implements Closeable {
+    @Override
+    public void close() throws IOException {
+
+    }
 }
