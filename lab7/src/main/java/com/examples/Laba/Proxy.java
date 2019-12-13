a0354cd0e17db710a30852f49472751b30ac14da
Egorychev Artem <temych_2000@mail.ru>
2019-12-13 21:41:20 +0300

    Scripted auto-commit on change (2019-12-13 21:41:20) by gitwatch.sh

--- lab7/src/main/java/com/examples/Laba/Proxy.java
+++ lab7/src/main/java/com/examples/Laba/Proxy.java
@@ -1,14 +1,24 @@
 package com.examples.Laba;
 
+import org.zeromq.ZContext;
+
 import java.io.Closeable;
 import java.io.IOException;
 
 public class Proxy implements Closeable {
-    
+    public static void main(String[] args) {
+        try (
+                ZContext context = new ZContext();
+                Proxy proxy = new Proxy(context)
+        ){
+
+        }
+
+        
 
 
 
-    @Override
+                @Override
     public void close() throws IOException {
 
     }
