637356ccdecef0f76dff7fa5e2b7182cfab0b628
Egorychev Artem <temych_2000@mail.ru>
2019-12-14 01:04:24 +0300

    Scripted auto-commit on change (2019-12-14 01:04:24) by gitwatch.sh

--- lab7/src/main/java/com/examples/Laba/CacheDealer.java
+++ lab7/src/main/java/com/examples/Laba/CacheDealer.java
@@ -1,11 +1,14 @@
 package com.examples.Laba;
 
+import org.zeromq.ZContext;
+
 import java.io.Closeable;
 import java.io.IOException;
 import java.util.logging.Logger;
 
 public class CacheDealer implements Closeable {
     private static final Logger log = Logger.getLogger(CacheDealer.class.getName());
+    public ZContext context;
 
     @Override
     public void close() throws IOException {
