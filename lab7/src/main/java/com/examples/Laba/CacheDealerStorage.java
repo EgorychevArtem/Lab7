207450523eab6a914f3d89b2b8d6eddf5cfac2ff
Egorychev Artem <temych_2000@mail.ru>
2019-12-13 23:27:54 +0300

    Scripted auto-commit on change (2019-12-13 23:27:54) by gitwatch.sh

--- lab7/src/main/java/com/examples/Laba/CacheDealerStorage.java
+++ lab7/src/main/java/com/examples/Laba/CacheDealerStorage.java
@@ -5,6 +5,7 @@ import org.zeromq.ZFrame;
 import java.util.Map;
 import java.util.Optional;
 import java.util.TreeMap;
+import java.util.stream.Collectors;
 
 public class CacheDealerStorage {
     Map<String, CacheDealerMeta> dealers = new TreeMap<>();
@@ -13,6 +14,7 @@ public class CacheDealerStorage {
         dealers.values().stream()
                 .filter(d -> d.inside(id))
                 .filter(d -> !d.isAlive())
-
+                .collect(Collectors.toList())
+                .forEach(d -> dealers.remove(d.id.toString()));
     }
 }
