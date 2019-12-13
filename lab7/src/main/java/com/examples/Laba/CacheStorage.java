77765f79e445d7c182f73a52159375bad13c75a6
Egorychev Artem <temych_2000@mail.ru>
2019-12-13 23:38:02 +0300

    Scripted auto-commit on change (2019-12-13 23:38:02) by gitwatch.sh

--- lab7/src/main/java/com/examples/Laba/CacheStorage.java
+++ lab7/src/main/java/com/examples/Laba/CacheStorage.java
@@ -2,11 +2,11 @@ package com.examples.Laba;
 
 public class CacheStorage {
     int start, end;
-    int[] cache;
+    int[] cache = new int[end - start +1];
 
     public CacheStorage(int start, int end, int[] cache){
         this.start = start;
         this.end = end;
-        
+        this.cache
     }
 }
